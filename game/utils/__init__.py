e76d38789da66f561f107465ec3a697e61785041
ultrashiny <vasyl.123.000@gmail.com>
2025-12-04 20:17:58 +0000

    perf: apply caching in prompt loading

--- game/utils/__init__.py
+++ game/utils/__init__.py
@@ -1,3 +1,11 @@
 from . import config
 from . import misc
 from . import uids
+from .prompt_loader import (
+    load_prompt,
+    get_base_sys_prompt,
+    get_op_sys_prompt,
+    get_spy_sys_prompt,
+    get_rule_sys_prompt,
+    clear_prompt_cache,
+)
